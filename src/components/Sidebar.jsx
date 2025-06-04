2985b2528304f2b5d64153faee681cda761a17ee
lishaangral <lishaangral@gmail.com>
2025-06-04 14:12:15 +0530

    Adding API

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,34 +1,45 @@
-import {Stack} from '@mui/material';
-import {categories} from '../utils/constants';
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
 
-const selectedCategory = 'New';
-const Sidebar = () => {
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
   return (
     <Stack
-        direction="row"
-        sx = {{
-            overflowY: "auto",
-            height: {sx: 'auto', md: '95%'},
-            flexDirection: {md: 'column'},
-        }}
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
     >
-        {categories.map((category) => (
-            <button
-            className="category-btn"
+      {categories.map((category) => (
+        <button
+          className="category-btn"
+          onClick={() => setSelectedCategory(category.name)}
+          style={{
+            background: category.name === selectedCategory && "#FC1503",
+            color: "white",
+          }}
+          key={category.name}
+        >
+          <span
             style={{
-                background: category.name === selectedCategory && '#FC1503', 
-                color: 'white'
+              color: category.name === selectedCategory ? "white" : "red",
+              marginRight: "15px",
             }}
-            key = {category.name}
-            >
-
-                <span style = {{ color: category.name === selectedCategory ? 'white' : 'red', marginRight:'15px'}}>{category.icon}</span>
-                <span style = {{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
-            </button>
-        ))}
-
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{
+              opacity: category.name === selectedCategory ? "1" : "0.8",
+            }}
+          >
+            {category.name}
+          </span>
+        </button>
+      ))}
     </Stack>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
